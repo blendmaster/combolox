cad83a3ef812ae8f00e5feacda8a10cb790b2c32
blendmaster <steven@waywiser.org>
2023-05-19 14:57:02 -0600

    fix peg definitions, finish lox expression grammar
    the oneOrMore thingi s annoying, but Collector is in theory nicer
    than List. still has the "efficient" folding, even if it really doesn't
    matter for this library.

--- Lox.java
+++ Lox.java
@@ -1,8 +1,12 @@
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.BiFunction;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.Arrays;
+import java.util.Optional;
 
 class Lox {
   public static void main(String[] args) {
@@ -18,11 +22,12 @@ interface F4<A, B, C, D, T> { T apply(A a, B b, C c, D d); }
 // parsing expression grammar free applicative,
 // parses a stream of Ps into a T
 sealed interface Peg<P, T> permits Peg.Success, Peg.Fail, Peg.Terminal, Peg.Sequence, Peg.Choice, Peg.OneOrMore, Peg.Ref {
-  // matches a single value
-  record Terminal<P, T>(P value, Function<P, T> map) implements Peg<P,T> {}
+  // matches a P by predicate, mapped into the output type.
+  record Terminal<P, T>(Predicate<P> predicate, Function<P, T> map) implements Peg<P,T> {}
 
-  // succeeds parsing nothing, aka pure
+  // succeeds but parsing nothing, aka pure
   record Success<P, T>(T value) implements Peg<P, T> {}
+
   // always fails
   record Fail<P, T>(String errorMessage) implements Peg<P,T> {}
 
@@ -31,13 +36,15 @@ sealed interface Peg<P, T> permits Peg.Success, Peg.Fail, Peg.Terminal, Peg.Sequ
   // parses left, then if it fails, parses right.
   record Choice<P, T>(Peg<P,T> left, Peg<P,T> right) implements Peg<P,T> {}
 
-  // parse multiple of the same, folded together. List<A> is a bit more expressive 
-  // than needed but close enough in java. I think in haskell, the A would be
-  // Foldable or something.
-  record OneOrMore<P, A, T>(Peg<P, A> peg, Function<List<A>, T> fold) implements Peg<P,T> {}
-  
+  // Parse one or more of the same, collected into a final T using the stream Collector interface.
+  // which is close enough to Haskell Foldable and has useful helpers like Collectors.toList().
+  // Note this requires at matching one input, i.e. the collection will be non-empty. This is
+  // because you can recover zero-or-more from Choice(OneOrMore(), Success(empty)), but
+  // you can't recover one-or-more from Sequence(first, ZeroOrMore()). you can't get back the
+  // Accumulator type from the final T to prepend the first.
+  record OneOrMore<P, I, A, T>(Peg<P, I> peg, Collector<I, A, T> collector) implements Peg<P,T> {}
 
-  // the other way to do OneOrMore is
+  // another way to do OneOrMore is
   //
   // record Continue<P, A, T>(Peg<A> first, Function<A, Peg<P,T>> cont) implements Peg<P,T> {}
   //
@@ -54,20 +61,22 @@ sealed interface Peg<P, T> permits Peg.Success, Peg.Fail, Peg.Terminal, Peg.Sequ
   }
 
   // deriving Functor
+  // XXX java type system not powerful enough for some of the type parameters in here.
   @SuppressWarnings("unchecked")
-  default <P, A, R> Peg<P, R> map(Function<T, R> f) {
+  default <A, R, I> Peg<P, R> map(Function<T, R> f) {
     return switch (this) {
-        case Terminal(var value, var fmap) -> new Terminal<P, R>(
-            ((P)value), ((Function<P, T>)fmap).andThen(f));
+        case Terminal(var predicate, var fmap) -> new Terminal<P, R>(
+            predicate, fmap.andThen(f));
         case Success(var value) -> new Success<>(f.apply(value));
         case Fail(var message) -> new Fail<>(message);
         case Sequence(var first, var next) ->
-          // XXX java type system not powerful enough for this.
           new Sequence<P, A, R>(
               ((Peg<P, A>)first), ((Peg<P, Function<A, T>>)next).map(f::compose));
         case Choice(var left, var right) -> new Choice<>(left.map(f), right.map(f));
-        case OneOrMore(var peg, var fold) -> 
-          new OneOrMore<>(((Peg<P, A>)peg), ((Function<List<A>, T>)fold).andThen(f));
+        case OneOrMore(var peg, var collector) -> 
+          new OneOrMore<P, I, A, R>(
+              ((Peg<P, I>)peg), Collectors.collectingAndThen(
+                ((Collector<I, A, T>)collector), f));
         // the peg AtomicReference is shared (and eventually set)
         case Ref(var peg, var map) -> new Ref(peg, map.andThen(f));
     }; 
@@ -76,7 +85,7 @@ sealed interface Peg<P, T> permits Peg.Success, Peg.Fail, Peg.Terminal, Peg.Sequ
   // helpers
 
   // convenience for when you just need a marker for whatever got parsed.
-  default <P, R> Peg<P, R> as(R r) {
+  default <R> Peg<P, R> as(R r) {
     return map(__ -> r);
   }
 
@@ -85,7 +94,7 @@ sealed interface Peg<P, T> permits Peg.Success, Peg.Fail, Peg.Terminal, Peg.Sequ
   }
 
   // fold multiple choices into a linked list, essentially.
-  @SuppressWarnings("unchecked")
+  @SafeVarargs
   static <P, T> Peg<P,T> choice(Peg<P,T> choice1, Peg<P,T> choice2, Peg<P,T>... more) {
     return Arrays.stream(more)
       .reduce(new Choice<>(choice1, choice2), (cs, c) -> new Choice<>(cs, c));
@@ -107,16 +116,40 @@ sealed interface Peg<P, T> permits Peg.Success, Peg.Fail, Peg.Terminal, Peg.Sequ
     return sequence((_1, a, _2) -> a, left, actual, right);
   }
 
+  static <P, T> Peg<P, Optional<T>> optional(Peg<P, T> peg) {
+    return choice(peg.map(Optional::of), new Success<>(Optional.empty()));
+  }
+
+  static Peg<Character, String> repeatString(Peg<Character, String> peg) {
+    return new OneOrMore<>(peg.map(a -> (CharSequence)a), Collectors.joining());
+  }
+
+  static <P, I, A, T> Peg<P, T> zeroOrMore(Peg<P, I> peg, Collector<I, A, T> collector) {
+    return choice(
+        new OneOrMore<>(peg, collector),
+        // kind of unfortunate we need to strictly create the empty case here, oh well
+        new Success<>(collector.finisher().apply(collector.supplier().get())));
+  }
+
+  static <P> Peg<P, P> matching(Predicate<P> predicate) {
+    return new Terminal<>(predicate, Function.identity());
+  }
+
+  static <P> Peg<P, P> literal(P value) {
+    return matching(value::equals);
+  }
+
   // extremely efficient string literal parser, character by character.
-  static Peg<Character, String> literal(String value) {
-    if (value.length() == 0) throw new AssertionError("blank literal");
-    Peg<Character,String> first = new Terminal<>(value.charAt(0), String::valueOf);
+  static Peg<Character, String> string(String value) {
+    if (value.length() == 1) {
+      return new Terminal<>(((Character)value.charAt(0))::equals, String::valueOf);
+    }
 
     // fold characters into a sequence of literals that foldss results back into a string.
     return value.chars()
-      .mapToObj(c -> (Peg<Character,String>) new Terminal<>((char)c, String::valueOf))
+      .mapToObj(c -> literal(Character.valueOf((char)c)).map(String::valueOf))
       .reduce(
-          first, 
+          new Success<>(""), 
           (prev, next) -> new Sequence<>(prev, next.map(n -> p -> p.concat(n))));
   }
 }
@@ -138,94 +171,88 @@ operator       → "==" | "!=" | "<" | "<=" | ">" | ">="
 could you write a Peg for the above language as Peg<Peg<?>>? yes, I think.
 */
 
-/*
-enum Operator {
-  EQUALS, NEQUALS, LESS, LESSEQUAL, GREATER, GREATEREQUAL, PLUS, MINUS, MULT, DIV
-}
+class LoxAst {
 
-record Binary(Expression left, Operator operator, Expression right) implements Expression {}
+  sealed interface Expression permits Literal, Unary, Binary, Grouping {}
 
-enum UnaryOp {
-  NEGATIVE, NOT
-}
-record Unary(UnaryOp op, Expression expression) implements Expression {}
-
-record Grouping(Expression expression) implements Expression {}
-
-sealed interface Literal extends Expression permits LoxNumber, LoxString, LoxTrue, LoxFalse, LoxNil {}
-record LoxNumber(float value) implements Literal {}
-record LoxString(String value) implements Literal {}
-record LoxTrue() implements Literal {}
-record LoxFalse() implements Literal {}
-record LoxNil() implements Literal {}
-
-class CombiLox {
-  // TODO ah, here's where you need the sort of lazy reference. the grammar
-  // is circular, though not directly recursive.
-  // reference would be a Peg<P,T> subclass that composes any functions mapped over.
-  // once reference is set, apply the functions.
-  // in interpreter for actual parsing, uh, does it need to know?
-  record LazyPeg<U, T>(AtomicReference<Peg<U>> underlying, Function<U, T> mapping) extends Peg<P,T> {
-    @Override
-    public <R> Peg<R> map(Function<T, R> f) {
-        return new LazyPeg<U, R>(underlying, mapping.andThen(f));
-    }
-
-    // eventually set the underlying, and uses need to apply the mapping.
-
+  enum Operator {
+    EQUALS, NEQUALS, LESS, LESSEQUAL, GREATER, GREATEREQUAL, PLUS, MINUS, MULT, DIV
   }
-  
-  static Peg<Expression> ExpressionPeg = new LazyPeg(new AtomicReference<Peg<Expression>>(), (t -> t));
 
-  static Peg<Literal> LiteralPeg = Pegs.choices(
-      Pegs.literalString("nil").map(__ -> new LoxNil()),
-      Pegs.literalString("true").map(__ -> new LoxTrue()),
-      Pegs.literalString("false").map(__ -> new LoxFalse()),
+  record Binary(Expression left, Operator operator, Expression right) implements Expression {}
 
-      Pegs.oneOrMore(
-      Pegs.choices("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"))
-      .map(digits -> new LoxNumber(parseFloat(digits.join("")))),
-      Pegs.surround(Pegs.alpha, "\"").map(LoxString::new)
+  enum UnaryOp {
+    NEGATIVE, NOT
+  }
+  record Unary(UnaryOp op, Expression expression) implements Expression {}
+
+  record Grouping(Expression expression) implements Expression {}
+
+  sealed interface Literal extends Expression permits LoxNumber, LoxString, LoxTrue, LoxFalse, LoxNil {}
+  record LoxNumber(float value) implements Literal {}
+  record LoxString(String value) implements Literal {}
+  record LoxTrue() implements Literal {}
+  record LoxFalse() implements Literal {}
+  record LoxNil() implements Literal {}
+
+  // Parser
+
+  static Peg.Ref<Character, Expression, Expression> ExpressionPeg = Peg.ref();
+
+  static Peg<Character, String> digit = 
+    Peg.<Character>matching(Character::isDigit).map(String::valueOf);
+
+  static Peg<Character, String> digits = Peg.repeatString(digit);
+
+  static Peg<Character, Float> number =
+    Peg.sequence(
+        (whole, decimal) -> Float.parseFloat(whole + decimal.orElse("")),
+        digits,
+        Peg.optional(
+          Peg.sequence(String::concat, Peg.string("."), digits)));
+
+  static Peg<Character, Literal> LiteralPeg = Peg.choice(
+      Peg.string("nil").as(new LoxNil()),
+      Peg.string("true").as(new LoxTrue()),
+      Peg.string("false").as(new LoxFalse()),
+      number.map(LoxNumber::new)
+      // TODO alpha
     );
 
-  static Peg<Unary> UnaryPeg = new Sequence<Unary>(
-      Pegs.choices(
-        Pegs.literalString("-").map(__ -> UnaryOp.NEGATIVE),
-        Pegs.literalString("!").map(__ -> UnaryOp.NOT)),
-      ExpressionPeg.map(exp -> op -> new Unary(op, exp)));
+  static Peg<Character, Unary> UnaryPeg = Peg.sequence(
+      Unary::new,
+      Peg.choice(
+        Peg.string("-").as(UnaryOp.NEGATIVE),
+        Peg.string("!").as(UnaryOp.NOT)),
+      ExpressionPeg);
 
+  static Peg<Character, Operator> OperatorPeg = Peg.choice(
+      Peg.string("==").as(Operator.EQUALS),
+      Peg.string("!=").as(Operator.NEQUALS),
+      Peg.string("<") .as(Operator.LESS),
+      Peg.string("<=").as(Operator.LESSEQUAL),
+      Peg.string(">") .as(Operator.GREATER),
+      Peg.string(">=").as(Operator.GREATEREQUAL),
+      Peg.string("+") .as(Operator.PLUS),
+      Peg.string("-") .as(Operator.MINUS),
+      Peg.string("*") .as(Operator.MULT),
+      Peg.string("/") .as(Operator.DIV));
 
-  static Peg<Operator> OperatorPeg = Pegs.choices(
-      Pegs.literalString("==").map(__ -> EQUALS),
-      Pegs.literalString("!=").map(__ -> NEQUALS),
-      Pegs.literalString("<").map(__ -> LESS),
-      Pegs.literalString("<=").map(__ -> LESSEQUAL),
-      Pegs.literalString(">").map(__ -> GREATER),
-      Pegs.literalString(">=").map(__ -> GREATEREQUAL),
-      Pegs.literalString("+").map(__ -> PLUS),
-      Pegs.literalString("-").map(__ -> MINUS),
-      Pegs.literalString("*").map(__ -> MULT),
-      Pegs.literalString("/").map(__ -> DIV)
-    );
-
-  static Peg<Binary> BinaryPeg = new Sequence<Binary>(
+  static Peg<Character, Binary> BinaryPeg = Peg.sequence(
+      Binary::new,
       ExpressionPeg,
-      new Sequence<Function<Expression, Unary>>(
-        OperatorPeg,
-        ExpressionPeg.map(rightExp -> op -> leftExp -> new Unary(leftExp, op, rightExp))
-        )
-      );
+      OperatorPeg,
+      ExpressionPeg);
 
-  static Peg<Grouping> GroupingPeg = 
-    // TODO or sequence, then list.get(1)
-    Pegs.surround(ExpressionPeg, "()").map(Grouping::new);
+  static Peg<Character, Grouping> GroupingPeg = 
+    Peg.surround(Peg.string("("), ExpressionPeg, Peg.string(")")).map(Grouping::new);
 
   {
-    ExpressionPeg.underlying.set(Pegs.choices(
-          LiteralPeg,
-          UnaryPeg,
-          BinaryPeg,
-          GroupingPeg));
+    // XXX requires upcast for some reason.
+    ExpressionPeg.set(Peg.<Character, Expression>choice(
+          LiteralPeg.map(a -> (Expression)a),
+          UnaryPeg.map(a -> (Expression)a),
+          BinaryPeg.map(a -> (Expression)a),
+          GroupingPeg.map(a -> (Expression)a)));
   }
 }
-*/
